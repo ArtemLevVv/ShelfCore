d0e73ac608baeebf2325b1b585fbc669a8f77915
ArtemLevVv <arzona.art@gmail.com>
2026-07-17 23:19:14 +0200

    design improvement

--- ShelfCore/auth_app/forms.py
+++ ShelfCore/auth_app/forms.py
@@ -10,3 +10,8 @@ class SignupForm(UserCreationForm):
             'password2',
             'email'
         ] 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].help_text = ""
+        self.fields["password1"].help_text = ""
+        self.fields["password2"].help_text = ""
